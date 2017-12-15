f0d2034d2ff8c250dafc6a795786ee9799e4bf8a
Holly Erickson <Erickson.Holly@icloud.com>
2017-12-15 01:19:04 -0600

    Functional About pages

--- data/projects.js
+++ data/projects.js
@@ -1,4 +1,5 @@
-function project(name, techs, description, link) {
+function project(tag, name, techs, description, link) {
+  this.tag = tag
   this.name = name
   this.techs = techs
   this.description = description
@@ -7,12 +8,12 @@ function project(name, techs, description, link) {
 }
 
 var projects = [
-  new project("This Portfolio!", ["Express", "Node.js", "JSON", "JavaScript", "HTML", "SASS"], "Use Express to populate my app with data on projects I've created", "https://github.com/Holly-E/portfolio_Express"),
-  new project("Music Player", ["JavaScript", "jQuery", "HTML", "CSS", "Bootstrap"], "Build an interactive, functional music player", "https://github.com/Holly-E/assignment_royalty_free_music_player"),
-  new project("jQuery Events and Effects", ["JavaScript", "jQuery", "HTML", "CSS"], "3 exercises practicing dynamic jQuery, including a photo tagging app", "https://github.com/Holly-E/assignment_jq_ee_sprint/tree/master/the_dropdown"),
-  new project("Blog template", ["Sass", "CSS", "HTML"], "Practice Sass and responsive design by recreating a picture of a blog example", "https://github.com/Holly-E/assignment_markup_warmup"),
-  new project("Node.js Server", ["Node.js", "JavaScript", "HTML"], "Use node.js to create a server run on a local host and fetch resquest/response data", "https://github.com/Holly-E/build_nodejs_server"),
-  new project("Viking Garden", ["CSS3" "HTML5"], "Use CSS to style site, responsive to change in window size", "https://github.com/Holly-E/assignment_viking_css_garden"),
+  new project("Portfolio", "This Portfolio!", ["Express", "Node.js", "JSON", "JavaScript", "HTML", "SASS"], "Use Express to populate my app with data on projects I've created", "https://github.com/Holly-E/portfolio_Express"),
+  new project("Player", "Music Player", ["JavaScript", "jQuery", "HTML", "CSS", "Bootstrap"], "Build an interactive, functional music player", "https://github.com/Holly-E/assignment_royalty_free_music_player"),
+  new project("jQueryEE", "jQuery Events and Effects", ["JavaScript", "jQuery", "HTML", "CSS"], "3 exercises practicing dynamic jQuery, including a photo tagging app", "https://github.com/Holly-E/assignment_jq_ee_sprint/tree/master/the_dropdown"),
+  new project("Blog", "Blog template", ["Sass", "CSS", "HTML"], "Practice Sass and responsive design by recreating a picture of a blog example", "https://github.com/Holly-E/assignment_markup_warmup"),
+  new project("Server", "Node.js Server", ["Node.js", "JavaScript", "HTML"], "Use node.js to create a server run on a local host and fetch request/response data", "https://github.com/Holly-E/build_nodejs_server"),
+  new project("Garden", "Viking Garden", ["CSS3", "HTML5"], "Use CSS to style site, responsive to change in window size", "https://github.com/Holly-E/assignment_viking_css_garden"),
 ]
 
 module.exports = {
--- routes/about.js
+++ routes/about.js
@@ -2,15 +2,16 @@ var express = require('express');
 var router = express.Router();
 let projects = require('../data/projects').projects
 
-function findProject(name) {
-  return projects.find(function(project) {
-    return name == project.name
-  }
+ function findProject(tag) {
+   return projects.find(function(project) {
+     return tag == project.tag
+   })
 }
 
-router.get('/:name', function(req, res, next) {
-  var project = findProject(req.params.name)
+router.get('/:tag', function(req, res, next) {
+  var project = findProject(req.params.tag)
   res.render('about', {project: project});
+  next();
 });
 
 module.exports = router;
